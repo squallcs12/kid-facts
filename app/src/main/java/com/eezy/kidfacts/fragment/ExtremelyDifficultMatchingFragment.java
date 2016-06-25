37f124c4fab0d79fe9a519f37be794396fadd4e0
Bang Dao <daotranbang@gmail.com>
2016-06-25 16:02:06 +0700

    Hide all 3 items of a Case

--- app/src/main/java/com/eezy/kidfacts/fragment/ExtremelyDifficultMatchingFragment.java
+++ app/src/main/java/com/eezy/kidfacts/fragment/ExtremelyDifficultMatchingFragment.java
@@ -1,7 +1,21 @@
 package com.eezy.kidfacts.fragment;
 
+import android.graphics.Point;
+import android.widget.ImageView;
+
 import com.eezy.kidfacts.MainActivity;
+import com.eezy.kidfacts.KidFactsApplication;
+import com.eezy.kidfacts.R;
+import com.eezy.kidfacts.content.Content;
+import com.eezy.kidfacts.model.Case;
+import com.eezy.kidfacts.model.Pane;
 import com.eezy.kidfacts.util.Util;
+import com.eezy.kidfacts.view.PageTitleView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class ExtremelyDifficultMatchingFragment extends BaseMatchingFragment {
 
@@ -9,5 +23,79 @@ public class ExtremelyDifficultMatchingFragment extends BaseMatchingFragment {
 
     public static void launch(MainActivity activity) {
         Util.launchFragment(activity, new ExtremelyDifficultMatchingFragment(), 3, 3, TAG, true);
+    }
+
+    private List<Integer> caseIndices;
+    private int index;
+    private List<Integer> missingItems;
+
+    @Override
+    protected void setupResources() {
+        int n = Content.getCases().size();
+        caseIndices = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            caseIndices.add(i);
+        }
+        Collections.shuffle(caseIndices, KidFactsApplication.random);
+        index = 0;
+    }
+
+    @Override
+    protected void setTitle(PageTitleView pageTitleView) {
+        pageTitleView.setPageTitleColor(getResources().getColor(R.color.happy_title));
+        pageTitleView.setPageSubtitleColor(getResources().getColor(R.color.happy_subtitle));
+        pageTitleView.setPageTitle("Title");
+        pageTitleView.setPageSubTitle("Subtitle");
+    }
+
+    @Override
+    protected void setBackground(ImageView background) {
+        background.setImageResource(R.drawable.bk_happy);
+    }
+
+    @Override
+    protected List<Pane> fillInResources(List<Integer> resIds, Pane cause1, Pane cause2, Pane result) {
+        List<Pane> targetPanes = new ArrayList<>();
+        targetPanes.add(cause1);
+        targetPanes.add(cause2);
+        targetPanes.add(result);
+        return targetPanes;
+    }
+
+    @Override
+    protected List<Point> getConnectionPoints() {
+        List<Point> result = new ArrayList<>();
+        for (int i = 0; i < 3; ++i) {
+            result.add(new Point(i, missingItems.get(i)));
+        }
+        return result;
+    }
+
+    @Override
+    protected List<Integer> retrieveResources() {
+        List<Integer> result = new ArrayList<>();
+        Random r = KidFactsApplication.random;
+        List<Case> cases = Content.getCases();
+
+        Case currentCase = cases.remove((int) (caseIndices.get(index)));
+        index = (index + 1) % caseIndices.size();
+
+        List<Integer> items = currentCase.split();
+        missingItems = new ArrayList<>(items);
+
+        List<Integer> candidateItems = new ArrayList<>();
+        candidateItems.addAll(items);
+
+        List<Integer> temp = new ArrayList<>();
+        for (Case c : cases) {
+            temp.addAll(c.split());
+        }
+        Collections.shuffle(temp, r);
+
+        candidateItems.addAll(temp.subList(0, 2));
+        Collections.shuffle(candidateItems, r);
+
+        result.addAll(candidateItems);
+        return result;
     }
 }
